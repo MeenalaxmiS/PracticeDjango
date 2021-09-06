069bd95bfded1ad2c35b6543310e870f59814f60
Meenalaxmi <meenalaxmi@testpress.in>
2021-09-06 17:46:56 +0530

    Fix bug

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -11,17 +11,18 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+    def __str__(self):
+        return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.choice_text
 
-def __str__(self):
-    return self.choice_text
-
-
-def increment_vote(self):
-    self.votes += 1
-    self.save()
+    def increment_vote(self):
+        self.votes += 1
+        self.save()
